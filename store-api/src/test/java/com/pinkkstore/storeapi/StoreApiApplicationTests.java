72dcc863ba1cf225ea094fac8fe14a6251e86ef3
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 14:35:00 +0300

    add store-api

---
+++ store-api/src/test/java/com/pinkkstore/storeapi/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
