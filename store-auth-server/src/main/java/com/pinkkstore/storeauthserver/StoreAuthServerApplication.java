e3d69057ee94cb35efa9b90b3f2073d6c0bcaa9e
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 14:06:45 +0300

    add store-auth-server

---
+++ store-auth-server/src/main/java/com/pinkkstore/storeauthserver/StoreAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storeauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAuthServerApplication.class, args);
+	}
+
+}
---
+++ store-auth-server/src/test/java/com/pinkkstore/storeauthserver/StoreAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storeauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
