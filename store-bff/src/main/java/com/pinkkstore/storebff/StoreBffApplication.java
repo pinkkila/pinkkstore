3985e0b0db71631a7e391caf9c48a38c295b262f
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 14:18:51 +0300

    add store-bff

---
+++ store-bff/src/main/java/com/pinkkstore/storebff/StoreBffApplication.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBffApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBffApplication.class, args);
+    }
+    
+}
---
+++ store-bff/src/test/java/com/pinkkstore/storebff/StoreBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storebff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBffApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
