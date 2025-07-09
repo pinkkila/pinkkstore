18ae2c09088653c3ba3c0cc05c3de7e37eab0607
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 17:12:22 +0300

    add initial data configuration to the store-api

---
+++ api-db/data.sql
@@ -0,0 +1,6 @@
+delete from product;
+
+insert into product(id, product_name, product_desc, stock_qty, reserved_qty, price)
+values (99, 'banana poster', 'Really beautiful picture of bananaj', 45, 0, 19.95);
+
+SELECT setval('product_id_seq', (SELECT MAX(id) from product));
---
+++ api-db/schema.sql
@@ -0,0 +1,11 @@
+drop table if exists product;
+
+create table if not exists product
+(
+    id           bigserial primary key,
+    product_name text    not null,
+    product_desc text    not null,
+    stock_qty    integer not null,
+    reserved_qty integer not null,
+    price        decimal not null
+);
