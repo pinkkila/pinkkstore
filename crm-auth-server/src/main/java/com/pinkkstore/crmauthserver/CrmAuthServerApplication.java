7a98ed53c80dae1fcac2fc5ddd795c30d37eca7c
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 14:13:23 +0300

    add crm-auth-server

---
+++ crm-auth-server/src/main/java/com/pinkkstore/crmauthserver/CrmAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.crmauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmAuthServerApplication.class, args);
+	}
+
+}
---
+++ crm-auth-server/src/test/java/com/pinkkstore/crmauthserver/CrmAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.crmauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
