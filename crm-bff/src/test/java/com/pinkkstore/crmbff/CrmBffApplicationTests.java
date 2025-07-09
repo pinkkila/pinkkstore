e4870924e4ee2e3704153b260302950bebb8e90c
pinkkila <petteri.pinkkila@outlook.com>
2025-07-09 14:25:15 +0300

    add crm-bff

---
+++ crm-bff/src/test/java/com/pinkkstore/crmbff/CrmBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.crmbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBffApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
