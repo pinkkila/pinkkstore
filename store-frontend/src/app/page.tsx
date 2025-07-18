e0ebc1d161a8f729f0d25dfe170d38cc6a53181d
pinkkila <petteri.pinkkila@outlook.com>
2025-07-18 14:46:28 +0300

    add initial carousel config

--- store-frontend/src/app/page.tsx
+++ store-frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
+import HomeCarousel from "@/components/home-carousel";
+
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center mt-15">
       <h1 className="text-3xl lg:text-5xl font-bold">
         Welcome to <span className="text-fuchsia-500">Pinkk Store</span>
       </h1>
-
+      <HomeCarousel/>
     </main>
   );
 }
---
+++ store-frontend/src/components/home-carousel.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay";
+
+export default function HomeCarousel() {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className="w-full"
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+
+}
+
