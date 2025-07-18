3fce7cc21993bebf4f59594e3035de76eff824f6
pinkkila <petteri.pinkkila@outlook.com>
2025-07-18 17:29:27 +0300

    add carousel css

--- store-frontend/src/components/home-carousel.tsx
+++ store-frontend/src/components/home-carousel.tsx
@@ -1,46 +1,46 @@
-"use client"
+"use client";
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
 export default function HomeCarousel() {
   return (
-    <Carousel
-      opts={{
-        align: "start",
-        loop: true,
-      }}
-      className="w-full"
-      plugins={[
-        Autoplay({
-          delay: 2000,
-        }),
-      ]}
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
+    <div className="mt-25 md:w-full group md:px-16">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        className=""
+        plugins={[
+          Autoplay({
+            delay: 2000,
+            stopOnInteraction: false,
+            stopOnMouseEnter: true,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
               <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
+                <CardContent className="flex items-center justify-center h-50">
                   <span className="text-3xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
-
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="invisible md:group-hover:visible duration-200" />
+        <CarouselNext className="invisible md:group-hover:visible duration-200" />
+      </Carousel>
+     </div>
+  );
 }
-
