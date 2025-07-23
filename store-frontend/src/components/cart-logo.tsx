f5c26b7549a5a2fd1036302d2c0f7ecfcbd1fa37
pinkkila <petteri.pinkkila@outlook.com>
2025-07-24 02:50:46 +0300

    add product count to the cart logo

--- store-frontend/src/components/cart-logo.tsx
+++ store-frontend/src/components/cart-logo.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
+import { useCartContext } from "@/lib/hooks";
 
 export default function CartLogo() {
+  const { cart } = useCartContext();
+  const productTotal = cart?.totalQty ? cart.totalQty : 0;
+
   return (
-    <Button size="lg" className="rounded-3xl" asChild>
+    <Button size="lg" className="relative rounded-3xl" asChild>
       {/*For some reason a size does not change https://github.com/shadcn-ui/ui/issues/6316*/}
-      <Link href={"/cart"}><ShoppingCart className="!size-6"/></Link>
+      <Link href={"/cart"}>
+        <ShoppingCart className="!size-6" />
+        {productTotal > 0 && (
+          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+            {cart?.totalQty}
+          </span>
+        )}
+      </Link>
     </Button>
   );
 }
