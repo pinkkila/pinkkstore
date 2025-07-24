820b46093812951a5db13bf65ba654a257a6f6bd
pinkkila <petteri.pinkkila@outlook.com>
2025-07-24 20:07:31 +0300

    style product-categories component

--- store-frontend/src/components/product-categories.tsx
+++ store-frontend/src/components/product-categories.tsx
@@ -1,12 +1,14 @@
 import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 export default function ProductCategories() {
   return (
-    <section className="felx flex-col mt-20 w-5/6 ">
+    <section className="felx flex-col mt-20">
       <h2 className="text-2xl font-bold">Product Categories</h2>
-      <div className="flex mt-5 gap-10 flex-col md:flex-row">
+
+      <div className="flex flex-col lg:flex-row gap-5 justify-between mt-5">
         <CategoriesCard
           categoryName="Pictures"
           categoryDesc="Amazing AI generated Pics."
@@ -31,23 +33,24 @@ type CategoryPageProps = {
   categoryName: string;
   categoryDesc: string;
   categoryImageUrl: string;
+  className?: string;
 };
 
-function CategoriesCard({ categoryName, categoryDesc, categoryImageUrl }: CategoryPageProps) {
+function CategoriesCard({ categoryName, categoryDesc, categoryImageUrl, className}: CategoryPageProps) {
   return (
-    <Link href={`/categories/${categoryName.toLowerCase()}`}>
-      <Card className="bg-transparent">
+    <Link href={`/categories/${categoryName.toLowerCase()}`} >
+      <Card className={cn("bg-transparent h-30 w-88 md:w-72 xl:w-96", className)}>
         <CardContent className="flex">
           <Image
-            className="rounded-md"
+            className="rounded-md object-cover"
             src={categoryImageUrl}
             alt={`${categoryName} category image`}
             width={80}
             height={80}
           />
-          <div>
-            <h3>{categoryName}</h3>
-            <p>{categoryDesc}</p>
+          <div className="px-3">
+            <h3 className="text-lg font-semibold">{categoryName}</h3>
+            <p className="text-sm text-muted-foreground">{categoryDesc}</p>
           </div>
         </CardContent>
       </Card>
