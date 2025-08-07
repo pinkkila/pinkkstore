1df7c76d85cd57b2e7d9bc9f16583048ed9df06d
pinkkila <petteri.pinkkila@outlook.com>
2025-08-07 23:28:08 +0300

    add store-reverse-proxy

---
+++ store-reverse-proxy/src/main/java/com/pinkkstore/storereverseproxy/StoreReverseProxyApplication.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storereverseproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreReverseProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreReverseProxyApplication.class, args);
+	}
+
+}
---
+++ store-reverse-proxy/src/test/java/com/pinkkstore/storereverseproxy/StoreReverseProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkkstore.storereverseproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreReverseProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
