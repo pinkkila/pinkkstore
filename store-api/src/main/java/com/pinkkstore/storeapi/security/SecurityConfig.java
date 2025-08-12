dd9953db5aac2adb640f66b6c67cfb499c90990d
pinkkila <petteri.pinkkila@outlook.com>
2025-08-12 14:55:12 +0300

    refactor local dev to use one client and disable consent view

--- store-api/src/main/java/com/pinkkstore/storeapi/security/SecurityConfig.java
+++ store-api/src/main/java/com/pinkkstore/storeapi/security/SecurityConfig.java
@@ -21,6 +21,10 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/carts").hasAnyAuthority("SCOPE_cart.read")
+                        .requestMatchers(HttpMethod.PUT, "/carts").hasAnyAuthority("SCOPE_cart.write")
+                        .requestMatchers(HttpMethod.GET, "/orders/**").hasAnyAuthority("SCOPE_order.read")
+                        .requestMatchers(HttpMethod.POST, "/orders").hasAnyAuthority("SCOPE_order.write")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
                         .jwt(Customizer.withDefaults())
