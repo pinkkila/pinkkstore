c5edaf76e7c67203514a808ba57230b24ee42bc2
pinkkila <petteri.pinkkila@outlook.com>
2025-08-26 16:13:30 +0300

    add not-found.tsx

---
+++ store-frontend/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-156px)]">
+      <h2 className="text-lg">This page could not be found.</h2>
+    </main>
+  );
+}
