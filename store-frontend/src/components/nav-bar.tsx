5d65ea312380185244df6828510abc16ca3714e2
pinkkila <petteri.pinkkila@outlook.com>
2025-09-01 13:34:13 +0300

    refactor nav-bar to use (hidden or flex) to avoid flicker on the page load

--- store-frontend/src/components/nav-bar.tsx
+++ store-frontend/src/components/nav-bar.tsx
@@ -1,9 +1,5 @@
-"use client";
-
-import { useMediaQuery } from "@/hooks/use-media-query";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 const routes = [
   {
@@ -21,19 +17,8 @@ const routes = [
 ];
 
 export default function NavBar() {
-  const [isMounted, setIsMounted] = useState(false);
-  const isMobile = useMediaQuery("(max-width: 767px)");
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted || isMobile) {
-    return null;
-  }
-
   return (
-    <nav className="flex items-center justify-center gap-17 w-full bg-white/8 border-b border-zinc-200/10">
+    <nav className="hidden md:flex items-center justify-center gap-17 w-full bg-white/8 border-b border-zinc-200/10">
       {routes.map((route) => {
         return (
           <Button key={route.path} asChild variant="ghost" size="lg" className="rounded-2xl font-bold dark:hover:bg-transparent hover:underline">
