18e62df6ef33ce980b34cca15a1a138a8a752ed5
pinkkila <petteri.pinkkila@outlook.com>
2025-10-16 13:27:49 +0300

    refactor ad-carousel imgs to 16:9

--- store-frontend/src/components/ad-carousel.tsx
+++ store-frontend/src/components/ad-carousel.tsx
@@ -19,42 +19,42 @@ type AdCarouselProps = {
 
 const images = [
   {
-    src: "/images/banana.jpg",
+    src: "/images/carousel/banana-16-9.jpg",
     alt: "banana picture",
-    title: "Fresh Banana pic 19.95",
-    positionClassName: "absolute top-0 left-0",
+    title: "Fresh Banana 19.95",
+    positionClassName: "absolute top-0 left-2",
     titleClassName:
       "text-black text-2xl md:text-xl lg:text-2xl font-semibold p-3",
     link: "/product/99",
   },
   {
-    src: "/images/dog.jpg",
+    src: "/images/carousel/dog-16-9.jpg",
     alt: "dog picture",
     title: "Animal AI pictures starting at 9.99",
     positionClassName:
-      "absolute bottom-3 left-2 bg-gradient-to-t from-black/10 via-black/50 to-transparent rounded-md px-1",
+      "absolute bottom-3 left-13 bg-gradient-to-t from-black/10 via-black/50 to-transparent rounded-3xl ",
     titleClassName: "text-white text-xl md:text-lg font-semibold p-3",
     link: "/product/101",
   },
   {
-    src: "/images/spring.jpg",
+    src: "/images/carousel/spring-16-9.jpg",
     alt: "spring picture",
     title: "Spring collection 2025",
     positionClassName:
-      "absolute bottom-5 xl:bottom-10 left-3 bg-gradient-to-t from-black/10 via-black/50 to-transparent rounded-md",
-    titleClassName: "text-white text-3xl md:text-xl lg:text-3xl font-bold p-4",
+      "absolute bottom-5 left-1 bg-gradient-to-t from-black/3 via-black/50 to-transparent rounded-xl",
+    titleClassName: "text-white text-2xl md:text-xl lg:text-3xl font-bold p-4",
     link: "/product/105",
   },
   {
-    src: "/images/orange.jpg",
+    src: "/images/carousel/orange-16-9.jpg",
     alt: "orange picture",
     title: "Sweet Orange from AI world",
-    positionClassName: "absolute bottom-0 left-0",
+    positionClassName: "absolute -top-3 left-10",
     titleClassName: "text-white text-2xl lg:text-2xl font-semibold p-3",
     link: "/product/100",
   },
   {
-    src: "/images/desert.jpg",
+    src: "/images/carousel/desert-16-9.jpg",
     alt: "desert picture",
     title: "Travel to the Desert AI experience",
     positionClassName: "absolute",
@@ -83,7 +83,7 @@ export default function AdCarousel({ className }: AdCarouselProps) {
             <CarouselItem key={img.src} className="md:basis-1/2 lg:basis-1/3 pl-3">
               <Card className="p-0">
                 <Link href={img.link}>
-                  <CardContent className="relative aspect-square">
+                  <CardContent className="relative aspect-video">
                     <Image
                       src={img.src}
                       alt={img.alt}
