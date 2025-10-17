9e74aae77c7daf33bb239e62cc29fe24c62f817a
pinkkila <petteri.pinkkila@outlook.com>
2025-10-17 11:56:11 +0300

    add logo

--- store-frontend/src/app/page.tsx
+++ store-frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex flex-col items-center gap-8">
       <div className="text-center">
         <h1 className="text-3xl lg:text-5xl font-bold">
-          Welcome to <span className="text-fuchsia-500">PinkkStore</span>
+          Welcome to <span className="text-[#FF00D0]">PinkkStore</span>
         </h1>
         <p className="text-lg lg:text-2xl opacity-75">
           We have Amazing AI generated pictures.
--- store-frontend/src/components/header.tsx
+++ store-frontend/src/components/header.tsx
@@ -2,14 +2,15 @@ import Link from "next/link";
 import AccountLoginNavigation from "@/components/account-login-navigation";
 import CartLogo from "@/components/cart-logo";
 import DrawerMenu from "@/components/drawer-menu";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between border-b border-zinc-200/30 h-18 px-4 sm:px-9">
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4 xl:gap-6">
         <DrawerMenu />
         <Link href="/">
-          <h2 className="text-xl md:text-3xl text-fuchsia-500">PinkkStore</h2>
+          <Image src="pinkkstore-logo.svg" alt="PinkkStore logo" height={0} width={0} className="h-8 xl:h-10 w-auto" />
         </Link>
       </div>
       <div className="flex items-center gap-1 lg:gap-3 ">
